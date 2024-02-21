64208dbadf94b0594d4f6983b33ed9d1b7a35fee
DeveloperSpoot <52226518+DeveloperSpoot@users.noreply.github.com>
2024-02-21 15:36:30 -0800

    Added comments to said completed application.

--- Problems/Sales Analysis.cs
+++ Problems/Sales Analysis.cs
@@ -9,45 +9,51 @@
 
         private void loadFileButton_Method(object sender, EventArgs e)
         {
-            double total = 0.00;
+            double total = 0.00; // initalizing a total double vairable.
 
-            try
+            try // Catching any incompetence.
             {
 
-                if(openFileDialog1.ShowDialog() == DialogResult.OK)
+                if(openFileDialog1.ShowDialog() == DialogResult.OK) // Show the user the file dialog and get the selected file.
                 {
-                    StreamReader inputFile = new StreamReader(openFileDialog1.FileName);
+                    StreamReader inputFile = new StreamReader(openFileDialog1.FileName); // Opening the selected file.
 
-                    double[] inputedSales = new double[100];
+                    double[] inputedSales = new double[100]; // Initalizing a new double array, and assuming there wont be over 100 sales. Could add an input so the user can increase/decrease;
 
-                    int lineIdex = 0;
+                    int lineIdex = 0; // Tracking the current index of of the file and the number of items in the sales file.
 
-                    while (!inputFile.EndOfStream)
+                    while (!inputFile.EndOfStream) // Looping through said file.
                     {
-                        double currentSale = double.Parse(inputFile.ReadLine());
-                        inputedSales[lineIdex] = currentSale;
-                        lineIdex++;
+                        double currentSale = double.Parse(inputFile.ReadLine()); // Reading the current line of said file.
+                        inputedSales[lineIdex] = currentSale; // Adding said sale to our array.
+                        lineIdex++; // Updating our index.
 
-                        total += currentSale;
+                        total += currentSale; // Updating our total.
 
-                        salesListBox.Items.Add(currentSale.ToString());
+                        salesListBox.Items.Add(currentSale.ToString()); // Adding said sale to the display list box.
                     }
 
+                    inputFile.Close(); // Closing the selected file.
+
+                    //Delcaring double vairables to hold the highest and lowest values of our array.
                     double highest = inputedSales[0];
                     double lowest = inputedSales[0];
 
-                    for(int i = 0; i < lineIdex; i++)
+                    for(int i = 0; i < lineIdex; i++)//Hey look at that, using the index as a refernce for the total amount of actual inputed sales. To figure out lowest and highest values.
                     {
-                        if (inputedSales[i] > highest) {
+                        //Sorting for the highest value.
+                        if (inputedSales[i] > highest) { 
                             highest = inputedSales[i];
                         }
 
+                        //Sorting for the lowest value.
                         if (inputedSales[i] < lowest)
                         {
                             lowest = inputedSales[i];
                         }
                     }
 
+                    //Updating all of our display labels.
                     totalLabel.Text = total.ToString("c");
                     averageLabel.Text = (total/lineIdex).ToString("c");
                     largestLabel.Text = highest.ToString("c");
@@ -57,11 +63,12 @@
 
             }catch(Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.Message); // Displaying how the user is being incompetent.
             }
 
         }
 
+        //Exit button method. Need anything else be mentioned?
         private void exitButton_Method(object sender, EventArgs e)
         {
             this.Close();
