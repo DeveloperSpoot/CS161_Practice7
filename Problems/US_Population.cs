f612ba2a9bb85ca3fea975c31ab16e1b155c4c95
DeveloperSpoot <52226518+DeveloperSpoot@users.noreply.github.com>
2024-03-04 15:37:52 -0800

    Finished

--- Problems/US_Population.cs
+++ Problems/US_Population.cs
@@ -9,6 +9,7 @@
         private void US_Population_Load(object sender, EventArgs e)
         {
             List<int> usPOP = new List<int>();
+
             try
             {
                 StreamReader popFile = File.OpenText("USPopulation.txt");
@@ -21,17 +22,13 @@
                 popFile.Close();
 
                 int greatestYear = 1950;
-                int greatesAmount = usPOP[0];
+                int greatesAmount = 0;
 
                 int leastYear = 1950;
-                int leastsAmount = usPOP[0];
+                int leastsAmount = 100000000;
 
-                int average = 0 ;
+                double average = 0.00 ;
                 int totalChange = 0;
-
-                int LastChange = 0;
-
-                int lastIndex = 0;
 
                 /*
                  * IDK WHAt TO DO, TAKE A BREAK NATHANIEL......
@@ -46,10 +43,46 @@
 
                 List<int> increaseChanges = new List<int>();
 
+                for(int i = 1; i <= usPOP.Count-1; i++)
+                {
+                    int changeIncreaseAmount = 0;
+                   
+                   changeIncreaseAmount = Math.Abs(usPOP[i] - usPOP[i -1 ]);
+                    System.Diagnostics.Debug.WriteLine(changeIncreaseAmount);
+                   increaseChanges.Add(changeIncreaseAmount);
+                    
 
+                    totalChange += changeIncreaseAmount;
+
+                    if(changeIncreaseAmount > greatesAmount)
+                    {
+                        greatestYear = 1950 + i;
+                        greatesAmount = changeIncreaseAmount;
+                    }
+
+                    if(changeIncreaseAmount < leastsAmount)
+                    {
+                        leastYear = 1950 + i;
+                        leastsAmount = changeIncreaseAmount;
+                    }
+                }
+
+                System.Diagnostics.Debug.WriteLine("<<< TOTAL >>>");
+                System.Diagnostics.Debug.WriteLine(totalChange);
+
+                System.Diagnostics.Debug.WriteLine("<<< CHANGE COUNT >>>");
+                System.Diagnostics.Debug.WriteLine(increaseChanges.Count);
+
+                average = (((double)totalChange) / (double)increaseChanges.Count)*1000.00;
+
+                lowestChangeLabel.Text = leastYear.ToString();
+                highestChangeLabel.Text = greatestYear.ToString();
+
+                averageLabel.Text = average.ToString("n2");
             }
             catch (Exception ex)
             {
+                System.Diagnostics.Debug.WriteLine(ex);
                 MessageBox.Show(ex.Message);
             }
         }
